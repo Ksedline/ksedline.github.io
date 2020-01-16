a3fb3c06478a7a16f7af97c0f6f89d7be5c6a786
KShekhovtsov <ksedline@gmail.com>
2020-01-16 15:36:55 +0300

    Doshik

---
+++ precache-manifest.98719d0665251721baa5d0475359cb6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cf0c7caa2babc1f164b4d2ffdf174a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e57e8a1480f15679d6fb",
+    "url": "/static/css/main.095245c6.chunk.css"
+  },
+  {
+    "revision": "4195f00c1f14fe5a8dc3",
+    "url": "/static/js/2.adaa8d5c.chunk.js"
+  },
+  {
+    "revision": "55d0b47d8b013b455d198284c65e90ca",
+    "url": "/static/js/2.adaa8d5c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e57e8a1480f15679d6fb",
+    "url": "/static/js/main.454aed97.chunk.js"
+  },
+  {
+    "revision": "f0827fd94c390f290d9b",
+    "url": "/static/js/runtime-main.2cf1115e.js"
+  }
+]);
