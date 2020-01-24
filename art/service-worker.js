078a044d96781d9b5fd6d386b59d5f03a6325e9d
Kirill Shekhovtsov <ksedline@gmail.com>
2020-01-24 03:31:59 +0300

    Fix homepage

--- art/service-worker.js
+++ art/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.986e042589b391c9920cd2b6f85acd59.js"
+  "art/precache-manifest.da20cba25c89ccfb3978c7e14309c1f1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("art/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
