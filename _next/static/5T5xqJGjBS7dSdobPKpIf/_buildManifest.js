c4041468b016550127f7f3acbf506a09a2d40f5c
Kirill Shekhovtsov <ksedline@gmail.com>
2020-04-19 23:29:34 +0300

    Replace Next with React

--- _next/static/5T5xqJGjBS7dSdobPKpIf/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a,b,c){return {"/":[a,b,c],"/index":[a,b,c]}}("static\u002Fchunks\u002Fb2e984c5.f6f9e7561dd0b70a73cc.js","static\u002Fcss\u002Fstyles.b01c79fe.chunk.css","static\u002Fchunks\u002Fstyles.a7be8643cdafda2928d5.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
